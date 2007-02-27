b3e77295a190dc196efcbd77275d9b2ba7116967
Martin Aspeli <optilude@gmail.com>
2007-02-27 16:58:14 +0000

    Add interface for the indexable object wrapper. The default implementation is still in Products.CMFPlone.CatalogTool
    svn path=/plone.app.content/trunk/; revision=13305

--- plone/app/content/interfaces.py
+++ plone/app/content/interfaces.py
@@ -11,3 +11,19 @@ class INameFromTitle(Interface):
 
 class IBatch(Interface):
     """A batch splits up a large number of items over multiple pages"""
+
+class IIndexableObjectWrapper(Interface):
+    """An adapter of a (object, portal) where object is to be indexed in 
+    portal_catalog.
+    
+    This should implement __getattr__(), which in turn should react
+    when the catalog tries to get attributes to index.
+    
+    The update() method must be called before the catalog is given the
+    wrapper.
+    """
+    
+    def update(vars, **kwargs):
+        """Update the wrapper with variables from e.g. the workflow
+        tool.
+        """
