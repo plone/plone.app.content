a7270b43f5544b677636978f2bbbae105e315f64
Hanno Schlichting <hanno@hannosch.eu>
2009-03-07 16:36:08 +0000

    Merged /plone.app.content/branches/1.x:r23483-25644
    svn path=/plone.app.content/trunk/; revision=25645

--- plone/app/content/browser/foldercontents.py
+++ plone/app/content/browser/foldercontents.py
@@ -1,11 +1,13 @@
 from zope.component import getMultiAdapter
 from zope.interface import implements
+from zope.i18n import translate
 
 from AccessControl import Unauthorized
 from Acquisition import aq_parent, aq_inner, aq_base
 from OFS.interfaces import IOrderedContainer
 from Products.CMFCore.utils import getToolByName
 from Products.CMFCore.interfaces import ISiteRoot
+from Products.CMFPlone.utils import safe_unicode
 from Products.Five import BrowserView
 
 
@@ -39,28 +41,29 @@ class FolderContentsView(BrowserView):
     def icon(self):
         """
         """
-        ploneview = getMultiAdapter((self.context, self.request), name="plone")
-        icon = ploneview.getIcon(self.context)
+        context = self.context
+        ploneview = getMultiAdapter((context, self.request), name="plone")
+        icon = ploneview.getIcon(context)
         return icon.html_tag()
 
     def parent_url(self):
         """
         """
-        portal_url = getToolByName(self.context, 'portal_url')
-        plone_utils = getToolByName(self.context, 'plone_utils')
-        portal_membership = getToolByName(self.context, 'portal_membership')
+        context = self.context
+        portal_url = getToolByName(context, 'portal_url')
+        plone_utils = getToolByName(context, 'plone_utils')
+        portal_membership = getToolByName(context, 'portal_membership')
 
-        obj = self.context
+        obj = context
 
         checkPermission = portal_membership.checkPermission
 
         # Abort if we are at the root of the portal
         if ISiteRoot.providedBy(self.context):
             return None
-        
 
         # Get the parent. If we can't get it (unauthorized), use the portal
-        parent = aq_parent(aq_inner(obj))
+        parent = aq_parent(obj)
         
         # # We may get an unauthorized exception if we're not allowed to access#
         # the parent. In this case, return None
@@ -69,7 +72,7 @@ class FolderContentsView(BrowserView):
                    parent.getId() == 'talkback':
                 # Skip any Z3 views that may be in the acq tree;
                 # Skip past the talkback container if that's where we are
-                parent = aq_parent(aq_inner(parent))
+                parent = aq_parent(parent)
 
             if not checkPermission('List folder contents', parent):
                 return None
@@ -112,7 +115,7 @@ class FolderContentsTable(object):
         self.request = request
         self.contentFilter = contentFilter
 
-        url = self.context.absolute_url()
+        url = context.absolute_url()
         view_url = url + '/@@folder_contents'
         self.table = Table(request, url, view_url, self.items,
                            show_sort_column=self.show_sort_column,
@@ -126,22 +129,24 @@ class FolderContentsTable(object):
     def items(self):
         """
         """
-        plone_utils = getToolByName(self.context, 'plone_utils')
-        plone_view = getMultiAdapter((self.context, self.request), name=u'plone')
-        context_state = getMultiAdapter((self.context, self.request),
+        context = self.context
+        plone_utils = getToolByName(context, 'plone_utils')
+        plone_view = getMultiAdapter((context, self.request), name=u'plone')
+        context_state = getMultiAdapter((context, self.request),
                                         name=u'plone_context_state')
-        portal_workflow = getToolByName(self.context, 'portal_workflow')
-        portal_properties = getToolByName(self.context, 'portal_properties')
+        portal_workflow = getToolByName(context, 'portal_workflow')
+        portal_properties = getToolByName(context, 'portal_properties')
+        portal_types = getToolByName(context, 'portal_types')
         site_properties = portal_properties.site_properties
         
         use_view_action = site_properties.getProperty('typesUseViewActionInListings', ())
-        browser_default = self.context.browserDefault()
-                
-        if IATTopic.providedBy(self.context):
-            contentsMethod = self.context.queryCatalog
+        browser_default = context.browserDefault()
+
+        if IATTopic.providedBy(context):
+            contentsMethod = context.queryCatalog
         else:
-            contentsMethod = self.context.getFolderContents
-        
+            contentsMethod = context.getFolderContents
+
         results = []
         for i, obj in enumerate(contentsMethod(self.contentFilter)):
             if (i + 1) % 2 == 0:
@@ -159,11 +164,16 @@ class FolderContentsTable(object):
             review_state = obj.review_state
             state_class = 'state-' + plone_utils.normalizeString(review_state)
             relative_url = obj.getURL(relative=True)
-            obj_type = obj.Type
+
+            type_title_msgid = portal_types[obj.portal_type].Title()
+            url_href_title = u'%s: %s' % (translate(type_title_msgid,
+                                                    context=self.request),
+                                          safe_unicode(obj.Description))
 
             modified = plone_view.toLocalizedTime(
                 obj.ModificationDate, long_format=1)
-            
+
+            obj_type = obj.Type
             if obj_type in use_view_action:
                 view_url = url + '/view'
             elif obj.is_folderish:
@@ -176,11 +186,11 @@ class FolderContentsTable(object):
                                  
             results.append(dict(
                 url = url,
+                url_href_title = url_href_title,
                 id  = obj.getId,
                 quoted_id = urllib.quote_plus(obj.getId),
                 path = path,
                 title_or_id = obj.pretty_title_or_id(),
-                description = obj.Description,
                 obj_type = obj_type,
                 size = obj.getObjSize,
                 modified = modified,
@@ -202,7 +212,7 @@ class FolderContentsTable(object):
     @property
     def orderable(self):
         """
-        """        
+        """
         return IOrderedContainer.providedBy(self.context)
 
     @property
@@ -220,9 +230,10 @@ class FolderContentsTable(object):
     @property
     def buttons(self):
         buttons = []
-        portal_actions = getToolByName(self.context, 'portal_actions')
+        context = self.context
+        portal_actions = getToolByName(context, 'portal_actions')
         button_actions = portal_actions.listActionInfos(
-            object=aq_inner(self.context), category='folder_buttons')
+            object=aq_inner(context), category='folder_buttons')
 
         # Do not show buttons if there is no data, unless there is data to be
         # pasted
@@ -236,7 +247,7 @@ class FolderContentsTable(object):
 
         for button in button_actions:
             # Make proper classes for our buttons
-            if button['id'] != 'paste' or self.context.cb_dataValid():
+            if button['id'] != 'paste' or context.cb_dataValid():
                 buttons.append(self.setbuttonclass(button))
         return buttons
 
