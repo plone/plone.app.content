c4ee54b05e75287f47b83c1172958b8ed81bebbd
Hanno Schlichting <hanno@hannosch.eu>
2009-05-08 17:33:05 +0000

    Removed the Plone UI related bits from this package by moving them into plone.app.layout.content. This package is reduced to providing bridge-code between CMF and Zope Toolkit-based content types.
    svn path=/plone.app.content/trunk/; revision=26811

--- README.txt
+++ README.txt
@@ -1,7 +1,5 @@
 Introduction
 ============
 
-plone.app.content contains various views for Plone, such as 
-folder_contents, as well as general content infrastructure, such as
-base classes and name choosers.
-
+plone.app.content contains general content infrastructure, such as base
+classes and name choosers.
--- plone/app/content/interfaces.py
+++ plone/app/content/interfaces.py
@@ -14,57 +14,6 @@ class IReindexOnModify(Interface):
     it's modified.
     """
 
-class IBatch(Interface):
-    """A batch splits up a large number of items over multiple pages"""
-
-    size = schema.Int(title=u"The amount of items in the batch")
-
-    firstpage = schema.Int(title=u"The number of the first page (always 1)")
-
-    lastpage = schema.Int(title=u"The number of the last page")
-
-    items_not_on_page = schema.List(
-        title=u"All items that are in the batch but not on the current page")
-
-    multiple_pages = schema.Bool(
-        title=u"Boolean indicating wheter there are multiple pages or not")
-
-    has_next = schema.Bool(
-        title=u"Indicator for wheter there is a page after the current one")
-
-    has_previous = schema.Bool(
-        title=u"Indicator for wheter there is a page after the current one")
-
-    previouspage = schema.Int(title=u"The number of the previous page")
-
-    nextpage = schema.Int(title=u"The number of the nextpage page")
-
-    next_item_count = schema.Int(title=u"The number of items on the next page")
-
-    navlist = schema.List(
-        title=u"List of page numbers to be used as a navigation list")
-
-    show_link_to_first = schema.Bool(
-        title=u"First page not in the navigation list")
-
-    show_link_to_last = schema.Bool(
-        title=u"Last page not in the navigation list")
-
-    second_page_not_in_navlist = schema.Bool(
-        title=u"Second page not in the navigation list")
-
-    before_last_page_not_in_navlist = schema.Bool(
-        title=u"Before last page not in the navigation list")
-
-    islastpage = schema.Bool(
-        title=u"Boolean indicating wheter the current page is the last page")
-
-    previous_pages = schema.List(
-        title=u"All previous pages that are in the navlist")
-
-    next_pages = schema.List(
-        title=u"All previous pages that are in the navlist")
-
 # XXX: This should be deprecated and removed in Plone 4.
 class IIndexableObjectWrapper(Interface):
     """An adapter of a (object, portal) where object is to be indexed in 
