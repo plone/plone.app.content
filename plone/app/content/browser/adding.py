6861bed4fdc51c293ab776e1b6eb16f5d8027c75
Hanno Schlichting <hanno@hannosch.eu>
2010-02-05 14:39:34 +0000

    Whitespace
    svn path=/plone.app.content/trunk/; revision=33744

--- plone/app/content/browser/adding.py
+++ plone/app/content/browser/adding.py
@@ -1,25 +1,25 @@
-from Products.Five.browser.adding import ContentAdding
 from Products.CMFCore.utils import getToolByName
+from Products.Five.browser.adding import ContentAdding
+
 
 class CMFAdding(ContentAdding):
     """An adding view with a less silly next-url
     """
-    
+
     # We need to do this to get proper traversal URLs - otherwise, the
     # <base /> tag is messed up.
     id = '+'
-    
+
     def add(self, content):
         content = super(CMFAdding, self).add(content)
-        
         # We need to ensure that we finish type construction, not at least
         # to set the correct permissions based on the workflow
         portal_types = getToolByName(content, 'portal_types')
         fti = portal_types.getTypeInfo(content)
         if fti is not None:
             fti._finishConstruction(content)
-        
+
         return content
-    
+
     def nextURL(self):
         return "%s/%s/view" % (self.context.absolute_url(), self.contentName)
