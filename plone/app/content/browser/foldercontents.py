d6daa04be0ab5645f91c0f17b503aaeef7190a4d
Thomas Desvenain <thomas.desvenain@gmail.com>
2011-06-21 15:12:34 +0000

    Replace links to .../@@folder_contents by links to .../folder_contents   so that 'Content' tab remains selected after a folder action. closes #10122.
    svn path=/plone.app.content/trunk/; revision=50608

--- plone/app/content/browser/foldercontents.py
+++ plone/app/content/browser/foldercontents.py
@@ -92,7 +92,7 @@ class FolderContentsTable(object):
         self.items = self.folderitems()
 
         url = context.absolute_url()
-        view_url = url + '/@@folder_contents'
+        view_url = url + '/folder_contents'
         self.table = Table(request, url, view_url, self.items,
                            show_sort_column=self.show_sort_column,
                            buttons=self.buttons)
