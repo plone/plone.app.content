fd65625339dbaa6fd5541f173149f63a9b651e76
David Glick <david@glicksoftware.com>
2014-02-10 10:08:40 +0100

    use a different attr to test acquisition now that main_template is a view

--- plone/app/content/browser/tests/test_adding.py
+++ plone/app/content/browser/tests/test_adding.py
@@ -11,11 +11,11 @@ class AddingTests(ptc.PloneTestCase):
     def test_adding_acquisition(self):
         adding = self.portal.unrestrictedTraverse('+')
         # Check explicit Acquisition
-        template = aq_get(adding, 'main_template')
+        template = aq_get(adding, 'portal_skins')
         self.assertTrue(template)
         # Check implicit Acquisition, unfortunately the CMF skins machinery
         # depends on this
-        template = getattr(adding, 'main_template')
+        template = getattr(adding, 'portal_skins')
         self.assertTrue(template)
         # Check traversal
         self.assertTrue(self.portal.unrestrictedTraverse('+/main_template'))
