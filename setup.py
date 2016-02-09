1782199cf70eb54c6d17d1daedd694496432ba4d
Maurits van Rees <maurits@vanrees.org>
2016-02-09 23:11:14 +0100

    Used https in pypi url.
    [ci skip]

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     keywords='plone content views viewlet',
     author='Plone Foundation',
     author_email='plone-developers@lists.sourceforge.net',
-    url='http://pypi.python.org/pypi/plone.app.content',
+    url='https://pypi.python.org/pypi/plone.app.content',
     license='GPL version 2',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['plone', 'plone.app'],
